0cb4f52087175764b2d8e70b3b28e59d0f2b265c
shradhay <shraddhayannawar16@gmail.com>
2020-09-28 06:57:17 -0700

    adding input field and button using material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { InputLabel,TextField,Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  return(
+    <>
+    <div>
+      <div>
+      <h1>Todo App</h1>
+      <form>
+        <InputLabel>Write Todo here</InputLabel>
+        <TextField></TextField>
+        <Button variant="contained" color="secondary">Add Todo</Button>
+        
+      </form>
+
+    
+
+      </div>
+     
     </div>
-  );
-}
 
+    </>
+  )
+}
 export default App;
