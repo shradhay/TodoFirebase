44fdd75e305bed7f1fcff8a3719040b8e911f8bd
shradhay <shraddhayannawar16@gmail.com>
2020-09-28 07:38:39 -0700

    css part done..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { InputLabel,TextField,Button } from '@material-ui/core';
+import "./App.css"
+import img from "./todo_img.png"
 
 const App=()=>{
   return(
     <>
-    <div>
-      <div>
-      <h1>Todo App</h1>
-      <form>
-        <InputLabel>Write Todo here</InputLabel>
-        <TextField></TextField>
-        <Button variant="contained" color="secondary">Add Todo</Button>
+   
+       <div className="maindiv">
+   
+        <div className="innerdiv">
+         <h1><img src={img}/>Todo App</h1>
+      
+     <form>
+        <InputLabel>Write Todo here...</InputLabel>
+        <TextField/>
+        <Button type="submit" variant="contained" color="secondary">Add Todo</Button>
         
       </form>
 
