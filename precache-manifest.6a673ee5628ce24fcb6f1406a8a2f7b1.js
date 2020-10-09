2b3a9d35cd06ded6fa26bcdbd93fef2a8e981ec3
shradhay <shraddhayannawar16@gmail.com>
2020-10-09 00:28:58 -0700

    Updates

---
+++ precache-manifest.6a673ee5628ce24fcb6f1406a8a2f7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe5983242ebdcfc911bf8708b0fba61",
+    "url": "/Todofirebase/index.html"
+  },
+  {
+    "revision": "43e8e3d4fde742d2424f",
+    "url": "/Todofirebase/static/css/main.3846ed52.chunk.css"
+  },
+  {
+    "revision": "406843809694faafbd5e",
+    "url": "/Todofirebase/static/js/2.32572543.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todofirebase/static/js/2.32572543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e8e3d4fde742d2424f",
+    "url": "/Todofirebase/static/js/main.17fe181d.chunk.js"
+  },
+  {
+    "revision": "ed3b658fd9f70f0ab9cb",
+    "url": "/Todofirebase/static/js/runtime-main.72e9396d.js"
+  },
+  {
+    "revision": "8cf708d271a8b98d0e251df3cef58384",
+    "url": "/Todofirebase/static/media/todo_img.8cf708d2.png"
+  }
+]);
